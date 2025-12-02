f7713cc7da82a328cdd6ed84053f6250b3692d85
ShobanMaryo <135240202+ShobanMaryo@users.noreply.github.com>
2025-12-02 23:44:07 +0530

    Remove initial App component implementation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,3 @@
-import React from "react";
-
-export default function App() {
-  return (
-    <div style={{ padding: 24 }}>
-      <h1>Resume Builder working!</h1>
-    </div>
-  );
-}
 import React, { useState, useRef } from "react";
 import { useReactToPrint } from "react-to-print";
 import { Plus, Trash2, Download, LayoutTemplate, Palette, Type } from "lucide-react";
